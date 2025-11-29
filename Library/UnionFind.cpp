f5193859b588eda2437e865f990b496c41103d9f
Pumpkin1e18 <pumpkin1e18@gmail.com>
2025-11-30 06:06:41 +0900

    UnionFindにsizeを追加

--- Library/UnionFind.cpp
+++ Library/UnionFind.cpp
@@ -2,32 +2,39 @@
 
 class UnionFind{
 public:
-	vector<int> ran, par;
+	vector<int> parent, depth, cnt;
 	void init(int n){
-		for(int i = 0;i < n;i++){
-			par.push_back(i);
-			ran.push_back(0);
+		for(int i = 0; i < n; i++){
+			parent.push_back(i);
+			depth.push_back(0);
+			cnt.push_back(1);
 		}
 	}
 	
 	int find(int x){
-		if(par[x] == x)return x;
-		return par[x] = find(par[x]);
+		if(parent[x] == x) return x;
+		return parent[x] = find(parent[x]);
 	}
 	
 	void unite(int x, int y){
-		x = find(par[x]);
-		y = find(par[y]);
-		if(x == y)return;
-		if(ran[x] < ran[y]){
-			par[x] = y;
-		}else{
-			par[y] = x;
-			if(par[x] == par[y])ran[x]++;
+		x = find(x);
+		y = find(y);
+		if(x == y) return;
+		if(depth[x] < depth[y]){
+			parent[x] = y;
+			cnt[y] += cnt[x];
+		} else {
+			parent[y] = x;
+			cnt[x] += cnt[y];
+			if(depth[x] == depth[y]) depth[x]++;
 		}
 	}
 	
 	bool same(int x, int y){
-		return find(par[x]) == find(par[y]);
+		return find(x) == find(y);
+	}
+	
+	int size(int x){
+		return cnt[find(x)];
 	}
 };
